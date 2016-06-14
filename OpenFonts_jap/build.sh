45c7c0e56170c6df92a3ae08eeaf49a949946f8e
Bektur <ryskeldiev.b@gmail.com>
2016-06-14 17:17:10 +0600

    Added Japanese comments

--- OpenFonts_jap/build.sh
+++ OpenFonts_jap/build.sh
@@ -1,6 +1,6 @@
 xelatex deedy_resume-openfont.xtx
 bibtex deedy_resume-openfont.aux
 xelatex deedy_resume-openfont.xtx
-cp -f deedy_resume-openfont.pdf RyskeldievB_CV.pdf
-open RyskeldievB_CV.pdf
-convert -density 300 -background white -alpha remove RyskeldievB_CV.pdf sample-image.png
+cp -f deedy_resume-openfont.pdf RyskeldievB_CV_JP.pdf
+open RyskeldievB_CV_JP.pdf
+convert -density 300 -background white -alpha remove RyskeldievB_CV_JP.pdf sample-image.png
